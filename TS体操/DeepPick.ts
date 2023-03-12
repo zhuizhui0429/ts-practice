74cdcefbe43a63293014b4149cf8f52efa42455b
“xiaoxiaoxian-ai” <2301766839@qq.com>
2023-03-12 16:08:31 +0800

    feat: 实现深度pick对象属性类型

--- TS体操/DeepPick.ts
+++ TS体操/DeepPick.ts
@@ -5,7 +5,6 @@ type MyPick<T extends object, U extends keyof T> = {
 interface Person {
     name: string
     age: number
-    hobby: string,
     address: {
         city: string,
     },
@@ -16,33 +15,51 @@ interface Person {
         }
     }
 }
-type test = MyPick<Person, 'hobby' | 'name'>
+type test = MyPick<Person, 'address' | 'name'>
 
+type JoinPrefix<P extends string, S extends string> = P extends '' ? S : `${P}.${S}`
+
+/**
+ * 得到一个对象类型的所有可能的属性path组成的联合类型
+ */
 type GetObjAllPaths<T extends Record<string, any>, Prefix extends string = '', Key extends keyof T = keyof T> =
     Key extends Key ? T[Key] extends object ?
-    GetObjAllPaths<T[Key], Prefix extends '' ? Key : `${Prefix}.${Key & string}`>
-    : Prefix extends '' ? Key : `${Prefix}.${Key & string}` : never
+    JoinPrefix<Prefix, Key & string> | GetObjAllPaths<T[Key], JoinPrefix<Prefix, Key & string>>
+    : JoinPrefix<Prefix, Key & string> : never
 
 type paths = GetObjAllPaths<Person>
 
 
-type Path2Keys<Path extends string, Res extends string[] = []> = Path extends `${infer key}.${infer Rest} ` ?
+type Path2Keys<Path extends string, Res extends string[] = []> = Path extends `${infer key}.${infer Rest}` ?
     Path2Keys<Rest, [...Res, key]> : [...Res, Path]
-
-type Keys2Path<K extends string[], Path extends string = ''> = K extends [infer Item extends string, ...infer Rest extends string[]] ?
-    Keys2Path<Rest, Path extends '' ? Item : `${Path}.${Item} `> : Path
 
 type keys = Path2Keys<'apple.color.test'>
 
+
+type Keys2Path<K extends string[], Path extends string = ''> = K extends [infer Item extends string, ...infer Rest extends string[]] ?
+    Keys2Path<Rest, JoinPrefix<Path, Item>> : Path
+
+type path = Keys2Path<['zzx', 'love', 'fitness']>
+
+
 type SingleDeepPick<T extends Record<string, any>, Path extends string, InitialPath extends string = Path, Keys extends string[] = Path2Keys<Path>> =
     Keys extends [infer Key extends string, ...infer Rest extends string[]] ?
-    Rest['length'] extends 0 ? Record<InitialPath, T[Path]> :
+    Rest['length'] extends 0 ? { [key in InitialPath]: T[Key] } :
     T[Key] extends object ? SingleDeepPick<T[Key], Keys2Path<Rest>, InitialPath> : never : never
 
-type test1 = SingleDeepPick<Person, 'friend.parent.name'>
+type test1 = SingleDeepPick<Person, 'friend.name'>
 
-type DeepPick<T extends Record<string, any>, Path extends GetObjAllPaths<T>> = Path extends Path ? SingleDeepPick<T, Path> : never
+type UnionToIntersection<U> =
+    (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
 
-type test2 = DeepPick<Person, 'name' | 'friend.parent' | 'friend.parent.name'>
+type DeepPick<T extends Record<string, any>, Path extends GetObjAllPaths<T>> =
+    UnionToIntersection<Path extends Path ? SingleDeepPick<T, Path> : never>
+
+
+
+type test2 = DeepPick<Person, 'friend.parent.name' | 'friend.parent' | 'age' | 'friend' | 'name'>
+
+
+
 
 export { }
